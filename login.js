cf772a1f09c20d4a7c714200a5200b0d4b760b24
Team 4226 <team4226@gmail.com>
2016-02-04 13:26:15 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function login(){
 	password = prompt("Please enter your unique page code:");
 	if(password=="933akY]KB5CCtY_t3!"){
-		confirm("93Thank you! Enjoy the stream.");
+		confirm("Thank you! Enjoy the stream.");
 	}
 	else if(password!=3akY]KB5CCtY_t3!)\{\
 		confirm("This page is protected by a passcode!");
